e81746b306856aa59037e276f99d6d7d3e354a5b
snahmd <sanahmed7@gmail.com>
2024-08-07 22:16:59 +0200

    add categories

--- blogpage/models.py
+++ blogpage/models.py
@@ -57,6 +57,32 @@ class BlogDetailAuthorPlacement(models.Model):
 
     def __str__(self):
         return self.author.name
+    
+class BlogCategories(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+register_snippet(BlogCategories)
+
+class BlogDetailCategoriesPlacement(models.Model):
+    page = ParentalKey('BlogDetail', related_name='categories_placement')
+    categories = models.ForeignKey(
+        'blogpage.BlogCategories',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+
+    panels = [
+        FieldPanel('categories'),
+    ]
+
+    def __str__(self):
+        return self.categories.name    
 
 class BlogIndex(Page):
 
@@ -88,8 +114,23 @@ class BlogIndex(Page):
     def get_context(self, request):
         context = super().get_context(request)
         context['blogpages'] = BlogDetail.objects.live().public().order_by('-first_published_at')
+        tag = request.GET.get('tag')
+        if tag:
+            # ignore case sensitive
+            if tag != "all":
+                context['blogpages'] = context['blogpages'].filter(tags__name__iexact=tag)
+        # filter category
+        category = request.GET.get('category')
+        if category:
+            # ignore case sensitive
+            if category != "all":
+                context['blogpages'] = context['blogpages'].filter(categories_placement__categories__slug__iexact=category)    
+
+        context["blog_categories"] = BlogCategories.objects.all()
         context["featuredOwner"] = "admin"
         context["myName"] = "San"
+        context["tag"] = tag if tag else "all"
+        context["category"] = category if category else "all"
 
         # Pagination
         page = request.GET.get('p')
@@ -120,6 +161,15 @@ class BlogPageTag(TaggedItemBase):
         on_delete=models.CASCADE
     )
 
+from wagtail.fields import StreamField
+from wagtail.blocks import TextBlock, BlockQuoteBlock, RichTextBlock
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.embeds.blocks import EmbedBlock
+from wagtailcodeblock.blocks import CodeBlock
+
+
+
+
 class BlogDetail(Page):
     
 
@@ -132,10 +182,10 @@ class BlogDetail(Page):
     
    
     subtitle = models.CharField(max_length=100, blank=True, null=True)
-    body = RichTextField(
-        blank=True,
-        features=['h2', 'h3', 'bold', 'italic', 'link', 'ol', 'ul', 'hr', 'document-link', 'image', 'embed', 'code', 'blockquote', 'superscript', 'subscript', 'strikethrough']
-    )
+    # body = RichTextField(
+    #     blank=True,
+    #     features=['h2', 'h3', 'bold', 'italic', 'link', 'ol', 'ul', 'hr', 'document-link', 'image', 'embed', 'code', 'blockquote', 'superscript', 'subscript', 'strikethrough']
+    # )
 
     tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
 
@@ -145,6 +195,21 @@ class BlogDetail(Page):
         blank=True,
         on_delete=models.SET_NULL,
         related_name='+'
+    )
+
+    body = StreamField(
+        [
+            ('text', TextBlock()),
+            ('rich_text', RichTextBlock()),
+            ('image', ImageChooserBlock()),
+            ('embed', EmbedBlock()),
+            ('code', CodeBlock()),
+            ('blockquote', BlockQuoteBlock()),
+
+        ],
+        use_json_field= True,
+        null=True,
+        blank=True,
     )
 
     cta_url_one = models.ForeignKey(
@@ -179,13 +244,14 @@ class BlogDetail(Page):
     content_panels = Page.content_panels + [
         FieldPanel('subtitle'),
         FieldPanel('body'),
-        FieldPanel('image'),
-        FieldPanel('cta_url_one'),
-        FieldPanel('cta_url_two'),
-        FieldPanel('cta_url_three'),
-        FieldPanel('cta_url_four'),
+        # FieldPanel('image'),
+        # FieldPanel('cta_url_one'),
+        # FieldPanel('cta_url_two'),
+        # FieldPanel('cta_url_three'),
+        # FieldPanel('cta_url_four'),
         FieldPanel('tags'),
-        InlinePanel('author_placement', label="Author"),
+        # InlinePanel('author_placement', label="Author"),
+        InlinePanel('categories_placement', label="Categories"),
     ] 
     # context ile template'e veri gönderme
     def get_context(self, request):
@@ -195,6 +261,7 @@ class BlogDetail(Page):
         # if prevPost exist then send it
         context["prevPost"] = BlogDetail.objects.live().public().filter(first_published_at__lt=self.first_published_at).order_by('-first_published_at').first()
         context["nextPost"] = BlogDetail.objects.live().public().filter(first_published_at__gt=self.first_published_at).order_by('first_published_at').first()
+        context["blog_categories"] = BlogCategories.objects.all()
         
 
         return context   
