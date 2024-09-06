b3c3352bc97fd627ee408de6c10448e95abda512
msdsn <devsanms@outlook.com>
2024-09-06 16:42:12 +0300

    Update production settings and requirements.txt
    - Update production settings to configure the database connection using dj_database_url.
    - Add ALLOWED_HOSTS to production settings.
    - Update requirements.txt to include psycopg2 and dj-database-url.

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -1,9 +1,18 @@
 from .base import *
 import os
-
+import dj_database_url
 env = os.environ.copy()
 
+DATABASES['default'] = dj_database_url.config(
+    default=env['DATABASE_URL'],
+    conn_max_age=600,
+    conn_health_checks=True,
+)
+
+
+
 SECRET_KEY = env["SECRET_KEY"]
+ALLOWED_HOSTS = ["*"]
 
 DEBUG = False
 
