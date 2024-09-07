79ee37f7cdf1300dd02924bed343b436f1c76544
snahmd <sanahmed7@gmail.com>
2024-09-07 13:13:42 +0200

    Update AWS storage bucket name and region

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -16,13 +16,11 @@ ALLOWED_HOSTS = ["*"]
 
 AWS_ACCESS_KEY_ID = env["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = env["AWS_SECRET_ACCESS_KEY"]
-AWS_STORAGE_BUCKET_NAME = 'djangolipsyncapp'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'
-AWS_S3_REGION_NAME = 'eu-north-1'  # Örneğin 'eu-central-1'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_STORAGE_BUCKET_NAME = 'yenideneme'
+AWS_S3_REGION_NAME = 'eu-central-1'  # Örneğin 'eu-central-1'
 
-MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/'
+
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{AWS_S3_REGION_NAME}/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEBUG = False
