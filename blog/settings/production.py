832133951cde028ef65432dd40fa2bea67a6c3af
snahmd <sanahmed7@gmail.com>
2024-09-07 13:37:52 +0200

    Disable AWS query string authentication

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -20,6 +20,7 @@ AWS_STORAGE_BUCKET_NAME = 'yenideneme'
 AWS_S3_REGION_NAME = 'eu-central-1'  # Örneğin 'eu-central-1'
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_QUERYSTRING_AUTH = False
 
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{AWS_S3_REGION_NAME}/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
