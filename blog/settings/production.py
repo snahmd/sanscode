1cda8c9bcfa7a7c54a7512fa6914752ed5e1933c
snahmd <sanahmed7@gmail.com>
2024-09-07 13:21:22 +0200

    Add AWS_DEFAULT_ACL configuration

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -18,7 +18,7 @@ AWS_ACCESS_KEY_ID = env["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = env["AWS_SECRET_ACCESS_KEY"]
 AWS_STORAGE_BUCKET_NAME = 'yenideneme'
 AWS_S3_REGION_NAME = 'eu-central-1'  # Örneğin 'eu-central-1'
-
+AWS_DEFAULT_ACL = 'public-read'
 
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{AWS_S3_REGION_NAME}/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
