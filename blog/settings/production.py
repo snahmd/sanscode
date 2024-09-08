6b3775302353fb493f1c189aa38e7f89d39db0bd
Ahmed San <35014146+snahmd@users.noreply.github.com>
2024-09-08 12:02:08 +0200

    Update production.py

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -25,7 +25,7 @@ AWS_QUERYSTRING_AUTH = False
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{AWS_S3_REGION_NAME}/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-DEBUG = False
+DEBUG = True
 
 try:
     from .local import *
